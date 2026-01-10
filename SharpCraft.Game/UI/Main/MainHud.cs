aed9a5bcc74e0272be2a1f6c5ddcaf3dd63724ac
Brandon Frie <brandon.frie@ejafi.com>
2026-01-10 04:03:19 -0500

    added crosshair rendering to HUD using ImGui

--- SharpCraft.Game/UI/Main/MainHud.cs
+++ SharpCraft.Game/UI/Main/MainHud.cs
@@ -1,4 +1,6 @@
-﻿using SharpCraft.Core;
+﻿using System.Numerics;
+using ImGuiNET;
+using SharpCraft.Core;
 using SharpCraft.Game.Controllers;
 
 namespace SharpCraft.Game.UI.Main;
@@ -9,6 +11,33 @@ public class MainHud : Hud
 
     public override void Draw(double deltaTime, World world, LocalPlayerController? player)
     {
+        var viewport = ImGui.GetMainViewport();
+        var center = viewport.GetCenter();
 
+        DrawCrosshair(center);
+    }
+
+    private static void DrawCrosshair(Vector2 position)
+    {
+        const float size = 10f;
+        const float thickness = 2f;
+        var color = ImGui.GetColorU32(new Vector4(1, 1, 1, 1));
+        var drawList = ImGui.GetForegroundDrawList();
+
+        // Vertical line
+        drawList.AddLine(
+            position with { Y = position.Y - size },
+            position with { Y = position.Y + size },
+            color,
+            thickness
+        );
+
+        // Horizontal line
+        drawList.AddLine(
+            position with { X = position.X - size },
+            position with { X = position.X + size },
+            color,
+            thickness
+        );
     }
 }
