4b6335cff695b54cfaf5859499ded4063a0c0b18
Brandon Frie <brandon.frie@ejafi.com>
2026-01-15 02:24:39 -0500

    Moved and reorganized test classes to align with project structure: removed outdated `SharpCraft.Sdk.Tests` test cases, migrated relevant tests for messaging and script execution to `SharpCraft.Engine.Tests`, and updated namespaces accordingly.

---
+++ tests/SharpCraft.Engine.Tests/Messaging/ChannelManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using AwesomeAssertions;
+using SharpCraft.Engine.Messaging;
+
+namespace SharpCraft.Engine.Tests.Messaging;
+
+public class ChannelManagerTests
+{
+    [Fact]
+    public void Messaging_ShouldPublishAndSubscribe()
+    {
+        var manager = new ChannelManager();
+        var channel = manager.GetChannel("test");
+        var received = false;
+
+        channel.Subscribe<string>(msg =>
+        {
+            msg.Should().Be("hello");
+            received = true;
+        });
+
+        channel.Publish("hello");
+
+        received.Should().BeTrue();
+    }
+}
---
+++ tests/SharpCraft.Engine.Tests/ScriptSandboxTests.cs
@@ -0,0 +1,18 @@
+﻿using AwesomeAssertions;
+using SharpCraft.Engine;
+
+namespace SharpCraft.Engine.Tests;
+
+public class ScriptSandboxTests
+{
+    [Fact]
+    public async Task ScriptSandbox_ShouldExecuteBasicScript()
+    {
+        var sandbox = new ScriptSandbox();
+        var code = "1 + 1";
+
+        var result = await sandbox.ExecuteAsync<int>(code);
+
+        result.Should().Be(2);
+    }
+}
